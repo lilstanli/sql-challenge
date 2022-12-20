cc1e9d3421e24686893753d5141abd8090735c3d
lilstanli <lilstanli@gmail.com>
2022-12-20 03:35:08 +0000

    Third commit finished second querry question

---
+++ EmployeeSQL/results/table_schemata.sql
@@ -0,0 +1,35 @@
+﻿CREATE TABLE Departments (
+    dept_no varchar(20) PRIMARY KEY,
+    dept_name varchar(50)
+);
+
+CREATE TABLE Titles (
+    title_id varchar(20) PRIMARY KEY,
+    title varchar(50)
+);
+
+CREATE TABLE Employees (
+    emp_no int PRIMARY KEY,
+    emp_title_id varchar(20) REFERENCES Titles(title_id),
+    birth_date varchar(20) NOT NULL,
+    first_name varchar(50) NOT NULL,
+    last_name varchar(50) NOT NULL,
+    sex varchar(20) NOT NULL,
+    hire_date varchar(20) NOT NULL
+);
+
+CREATE TABLE Dept_emp (
+    emp_no int REFERENCES Employees(emp_no),
+    dept_no varchar(20) REFERENCES Departments(dept_no)
+);
+
+CREATE TABLE Dept_manager (
+    dept_no varchar(20) REFERENCES Departments(dept_no),
+    emp_no int NOT NULL REFERENCES Employees(emp_no)
+);
+
+CREATE TABLE Salaries (
+    emp_no int REFERENCES Employees(emp_no),
+    salary money NOT NULL
+);
+
